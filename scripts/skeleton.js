8ad20459ab19ec556d9c3bb1943e1f97618a4917
YeEun <113398106+YeEun-Ha@users.noreply.github.com>
2022-10-26 11:27:55 -0800

    add navbar

---
+++ scripts/skeleton.js
@@ -0,0 +1,9 @@
+//---------------------------------------------------
+// This function loads the parts of your skeleton 
+// (navbar, footer, and other things) into html doc. 
+//---------------------------------------------------
+function loadSkeleton(){
+    console.log($('#navbarPlaceholder').load('./text/nav.html'));
+    console.log($('#footerPlaceholder').load('./text/footer.html'));
+}
+loadSkeleton();  //invoke the function
