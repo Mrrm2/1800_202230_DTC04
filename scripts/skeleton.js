aad3cc6678344d672b3db8fd700818b381b22e65
Joseph Chun <joseph.chun@gmail.com>
2022-10-26 13:46:25 -0700

    Fix bug and add template html

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,9 +1,9 @@
 //---------------------------------------------------
-// This function loads the parts of your skeleton 
-// (navbar, footer, and other things) into html doc. 
+// This function loads the parts of your skeleton
+// (navbar, footer, and other things) into html doc.
 //---------------------------------------------------
-function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./text/nav.html'));
-    console.log($('#footerPlaceholder').load('./text/footer.html'));
+function loadSkeleton() {
+  console.log($("#navbarPlaceholder").load("/text/nav.html"));
+  console.log($("#footerPlaceholder").load("/text/footer.html"));
 }
-loadSkeleton();  //invoke the function
+loadSkeleton(); //invoke the function
