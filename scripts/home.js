1677f093767de44a8b9cdf3ee4a74daa3105e921
Joseph Chun <83483639+Mrrm2@users.noreply.github.com>
2022-11-06 22:04:35 -0800

    Add personalization to homepage

--- scripts/home.js
+++ scripts/home.js
@@ -5,3 +5,18 @@ for (let input of listOfInput) {
     input.parentElement.parentElement.children[0].innerHTML = input.innerHTML;
   });
 }
+
+function displayName() {
+  // get user information from firebase
+  firebase.auth().onAuthStateChanged((user) => {
+    // Check if a user is signed in:
+    if (user) {
+      user_Name = user.displayName;
+      $("#welcomeBanner").text("Welcome, " + user_Name + "!"); //using jquery
+    } else {
+      $("#welcomeBanner").text("Welcome, Guest!");
+    }
+  });
+}
+
+displayName();
