ce17b360b7a1046e5a8899ada4f641a902c76ed0
Joseph Chun <83483639+Mrrm2@users.noreply.github.com>
2022-11-06 17:23:16 -0800

    Change navbar display based on user status

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,10 @@
-// function insertName() {
-//   firebase.auth().onAuthStateChanged((user) => {
-//     // Check if a user is signed in:
-//     if (user) {
-//       // Do something for the currently logged-in user here:
-//       // console.log(user.uid);
-//       console.log(user.displayName);
-//       user_Name = user.displayName;
-
-//       $(".welcomeBanner").text(user_Name.toUpperCase()); //using jquery
-//     } else {
-//       // No user is signed in.
-//     }
-//   });
-// }
-// insertName(); //run the function
+firebase.auth().onAuthStateChanged(function (user) {
+  const element = document.getElementById("nav1");
+  if (user) {
+    element.innerHTML = "My Account";
+    element.href = "/account.html";
+  } else {
+    element.innerHTML = "Sign In";
+    element.href = "/signin.html";
+  }
+});
