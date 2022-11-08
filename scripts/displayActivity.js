9aa389acd1b2d482ba38a6af5b67b58f5c5c9a69
YeEun <113398106+YeEun-Ha@users.noreply.github.com>
2022-11-07 23:09:15 -0800

    Add activity result page

--- scripts/displayActivity.js
+++ scripts/displayActivity.js
@@ -21,35 +21,3 @@ function readActivity() {
       })
 }
 readActivity();        //calling the function
-
-
-$("body").on("click", ".backdropBtn", function () {
-     console.log(`https://image.tmdb.org/t/p/w500/${$(this).attr('movieBackdropImageName')}`);
-     $("aside").html(
-   // $("main").append(
-       `
-         <img src="https://image.tmdb.org/t/p/w500/${$(this).attr('movieBackdropImageName')}"> 
-       `
-     )
-   })
-
-   for (i = 0; i < data.results.length; i++) {
-    $("main").append(
-      `
-      <div>
-        ${data.results[i].title}
-        <p>
-          ${data.results[i].overview}
-        </p>
-        <img 
-          src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}"
-          style="width: 100%" 
-        >
-        <button movieBackdropImageName="${data.results[i].backdrop_path}" class="backdropBtn"> BackDrop Image </button>
-        <hr>
-      </div>
-      `
-    ) // width: 100% - div 안에서만 있고 overflow되지 않게 함 
-    
-      ;
-  }
