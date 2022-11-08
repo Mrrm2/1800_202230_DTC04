50c9abfa751f5f9f7507af6cad61c205f37e0d69
Joseph Chun <83483639+Mrrm2@users.noreply.github.com>
2022-11-07 21:12:57 -0800

    Add function to store filters in local storage

--- scripts/home.js
+++ scripts/home.js
@@ -1,10 +1,39 @@
 const listOfInput = document.querySelectorAll(".dropdown-item");
+let userFilters = {
+  cost: "None",
+  time: "None",
+  proximity: "None",
+  group: "None",
+  energy: "None",
+  inout: "None",
+};
 
+// Function to change dropdown text to selection and put filters in the userFilters object
 for (let input of listOfInput) {
   input.addEventListener("click", function () {
-    input.parentElement.parentElement.children[0].innerHTML = input.innerHTML;
+    // Set the value and key to be the filter value and filter name
+    let value = input.innerHTML;
+    let key = input.parentElement.parentElement.parentElement.id;
+
+    // Change dropdown text to selected value
+    input.parentElement.parentElement.children[0].innerHTML = value;
+
+    // Add value and key into userFilters object
+    if (value === "----- None -----") {
+      value = "None";
+    }
+    userFilters[key] = value;
+    // console.log(userFilters);
   });
 }
+
+$("#generate").click(function () {
+  localStorage.setItem("filtersForGenerate", JSON.stringify(userFilters));
+
+  // add these to activity generation page
+  // console.log(JSON.parse(localStorage.getItem("filtersForGenerate")));
+  // window.onload = JSON.parse(localStorage.getItem("filtersForGenerate"));
+});
 
 function displayName() {
   // get user information from firebase
