53f78f9f6c1755e2ea3aa01216dd1b23ce44c73b
Joseph Chun <83483639+Mrrm2@users.noreply.github.com>
2022-11-22 11:35:24 -0800

    Add favourite and unfavourite button to favourite list

--- scripts/onClickFavor.js
+++ scripts/onClickFavor.js
@@ -1,12 +1,14 @@
 // If you wanna use local storage 🦄 - but doesn't work in this case anyway
 // var actName = localStorage.getItem("Paint and Sip"); // key랑 value가 똑같아서 사실 필요없음ㅋ
+var currActivity = '';
 
-function readActivity() {
+async function readActivity() {
 	let params = new URL(window.location.href);
 	// let activityID = params.searchParams.get("activityID")
 	let name = params.searchParams.get('name');
 
-	db.collection('activities_example')
+	await db
+		.collection('activities_example')
 		.where('name', '==', name)
 		.get()
 		.then((somedoc) => {
@@ -31,6 +33,7 @@ function readActivity() {
 				somedoc.docs[0].data().group;
 			document.getElementById('proximityGoesHere').innerHTML =
 				somedoc.docs[0].data().proximity;
+			currActivity = somedoc.docs[0].id;
 
 			//Here are other ways to access key:value data fields
 			//$('#quote-goes-here').text(tuesdayDoc.data().quote);                                       //using jquery object dot notation
@@ -38,10 +41,107 @@ function readActivity() {
 		});
 }
 
-readActivity();
-
 document.getElementById('backToFav').addEventListener('click', goBack);
 
 function goBack() {
 	history.back();
 }
+
+function setFavouriteButton() {
+	// for if activity is not favourite
+	$('#favourite').html(`
+      Favourite <i class='bi bi-star'></i>
+    `);
+}
+
+function setUnfavouriteButton() {
+	// for if activity is favourite
+
+	$('#favourite').html(`
+      Unfavourite <i class='bi bi-star-fill' style = 'color: rgb(255, 210, 48);'></i>
+      `);
+}
+
+function removeFavourite(ID, activity_ID) {
+	db.collection('users')
+		.doc(ID)
+		.set(
+			{
+				favourites: firebase.firestore.FieldValue.arrayRemove(activity_ID),
+			},
+			{ merge: true }
+		);
+}
+
+function addFavourite(ID, activity_ID) {
+	db.collection('users')
+		.doc(ID)
+		.set(
+			{
+				favourites: firebase.firestore.FieldValue.arrayUnion(activity_ID),
+			},
+			{ merge: true }
+		);
+}
+
+$('#favourite').click(function () {
+	console.log('clicked');
+	firebase.auth().onAuthStateChanged(function (user) {
+		if (user) {
+			user_ID = user.uid;
+			// check if activity id is in user's favourites
+			db.collection('users')
+				.doc(user_ID)
+				.get()
+				.then((doc) => {
+					if (doc.data().favourites != undefined) {
+						if (doc.data().favourites.includes(currActivity)) {
+							// remove currActivity from firestore
+							removeFavourite(user_ID, currActivity);
+							setFavouriteButton();
+						} else {
+							// add currActivity to database
+							addFavourite(user_ID, currActivity);
+							setUnfavouriteButton();
+						}
+					} else {
+						// add currActivity to database
+						addFavourite(user_ID, currActivity);
+						setUnfavouriteButton();
+					}
+				});
+		} else {
+			if (confirm('Please sign in to add to your favourites!')) {
+				window.location.href = '/signin.html';
+			}
+		}
+	});
+});
+
+function reactiveFavouriteButton() {
+	firebase.auth().onAuthStateChanged(function (user) {
+		if (user) {
+			user_ID = user.uid;
+			// check if activity id is in user's favourites
+			db.collection('users')
+				.doc(user_ID)
+				.get()
+				.then((doc) => {
+					if (
+						doc.data().favourites != undefined &&
+						doc.data().favourites.includes(currActivity)
+					) {
+						setUnfavouriteButton();
+					} else {
+						setFavouriteButton();
+					}
+				});
+		}
+	});
+}
+
+$(document).ready(async function () {
+	// check if user is logged in
+	await readActivity();
+	reactiveFavouriteButton();
+});
