fbd5be0dab8a30c526af103dc857a11953b58170
kirayoon <113317993+kirayoon@users.noreply.github.com>
2022-12-02 12:45:17 -0800

    Refactor and simplify functions

--- scripts/activitySubmissions.js
+++ scripts/activitySubmissions.js
@@ -1,51 +1,55 @@
 function displayList(collection, activityLength, submissionList) {
-	let submissionTemplate = document.getElementById('submission_template');
+  let submissionTemplate = document.getElementById('submission_template');
 
-	db.collection(collection)
-		.get()
-		.then((snap) => {
-			snap.forEach((doc) => {
-				var submissionID = doc.id;
+  db.collection(collection)
+    .get()
+    .then((snap) => {
+      snap.forEach((doc) => {
+        var submissionID = doc.id;
 
-				// iterate through each submissionID in favourites
-				for (var i = 0; i < activityLength; i++) {
-					if (submissionList[i] == submissionID) {
-						var name = doc.data().activityName;
-						var desc = doc.data().description;
-						let newcard = submissionTemplate.content.cloneNode(true);
+        // iterate through each submissionID in favourites
+        for (var i = 0; i < activityLength; i++) {
+          if (submissionList[i] == submissionID) {
+            var name = doc.data().activityName;
+            var desc = doc.data().description;
+            let newcard = submissionTemplate.content.cloneNode(true);
 
-						// update name and desc
-						newcard.querySelector('#activityName').innerHTML = name;
-						newcard.querySelector('#description').innerHTML = desc;
+            // update name and desc
+            newcard.querySelector('#activityName').innerHTML = name;
+            newcard.querySelector('#description').innerHTML = desc;
 
-						// attach
-						document.getElementById('favourites_group').appendChild(newcard);
-					}
-				}
-			});
-		});
+            // attach
+            document.getElementById('favourites_group').appendChild(newcard);
+          }
+        }
+      });
+    });
 }
 
 function getSubmissions() {
-	firebase.auth().onAuthStateChanged((user) => {
-		if (user) {
-			// get favourited activities' ID from user
-			currentUser = db.collection('users').doc(user.uid);
-			currentUser.get().then((userDoc) => {
-				var submissionList = userDoc.data().submissions;
-				activityLength = submissionList.length;
-
-				if (activityLength == 0) {
-					$('#favourites_group').html(
-						`<div style='text-align:center;padding-top:2em'> 
-			  	<h5> You haven't submitted any activities yet! </h5>`
-					);
-				}
-
-				displayList('community', activityLength, submissionList);
-			});
-		}
-	});
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // get favourited activities' ID from user
+      currentUser = db.collection('users').doc(user.uid);
+      currentUser.get().then((userDoc) => {
+        var submissionList = userDoc.data().submissions;
+        if (typeof submissionList === 'undefined') {
+          $('#favourites_group').html(
+            `<div style='text-align:center;padding-top:2em'>
+				<h5> You have no submissions yet! </h5>`
+          );
+        } else if (submissionList.length == 0) {
+          $('#favourites_group').html(
+            `<div style='text-align:center;padding-top:2em'> 
+					<h5> You haven't submitted any activities yet! </h5>`
+          );
+        } else {
+          activityLength = submissionList.length;
+          displayList('community', activityLength, submissionList);
+        }
+      });
+    }
+  });
 }
 
 getSubmissions();
--- scripts/favourite.js
+++ scripts/favourite.js
@@ -1,60 +1,59 @@
+function displayList(collection, activityLength, activityList) {
+  let favouritesTemplate = document.getElementById('favourites_template');
+
+  db.collection(collection)
+    .get()
+    .then((snap) => {
+      snap.forEach((doc) => {
+        var activityID = doc.id;
+
+        // iterate through each activityID in favourites
+        for (var i = 0; i < activityLength; i++) {
+          if (activityList[i] == activityID) {
+            var name = doc.data().name;
+            var desc = doc.data().description;
+            let newcard = favouritesTemplate.content.cloneNode(true);
+
+            // update name and desc
+            newcard.querySelector('#activityName').innerHTML = name;
+            newcard.querySelector('#description').innerHTML = desc;
+
+            //URL piggyback
+            newcard.querySelector('.read-more').href =
+              'onClickFavor.html?name=' + name;
+
+            // attach
+            document.getElementById('favourites_group').appendChild(newcard);
+          }
+        }
+      });
+    });
+}
+
 function getFavourites() {
-	firebase.auth().onAuthStateChanged((user) => {
-		if (user) {
-			// get favourited activities' ID from user
-			currentUser = db.collection('users').doc(user.uid);
-			currentUser.get().then((userDoc) => {
-				var activityList = userDoc.data().favourites;
-				activityLength = activityList.length;
-
-				function displayList(collection) {
-					let favouritesTemplate = document.getElementById(
-						'favourites_template'
-					);
-
-					if (activityLength == 0) {
-						$('#favourites_group').html(
-							`<div style='text-align:center;padding-top:2em'> 
-			  	<h5> You have no favourited activities yet! </h5>`
-						);
-					}
-
-					db.collection(collection)
-						.get()
-						.then((snap) => {
-							snap.forEach((doc) => {
-								var activityID = doc.id;
-
-								// iterate through each activityID in favourites
-								for (var i = 0; i < activityLength; i++) {
-									if (activityList[i] == activityID) {
-										var name = doc.data().name;
-										var desc = doc.data().description;
-										let newcard = favouritesTemplate.content.cloneNode(true);
-										// getActID(name,name) // 🦄In case you wanna use local storage
-
-										// update name and desc
-										newcard.querySelector('#activityName').innerHTML = name;
-										newcard.querySelector('#description').innerHTML = desc;
-
-										//URL piggyback 🐷
-										newcard.querySelector('.read-more').href =
-											'onClickFavor.html?name=' + name;
-										// +"&activityID=" + activityID;
-
-										// attach
-										document
-											.getElementById('favourites_group')
-											.appendChild(newcard);
-									}
-								}
-							});
-						});
-				}
-				displayList('activities_example');
-			});
-		}
-	});
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // get favourited activities' ID from user
+      currentUser = db.collection('users').doc(user.uid);
+      currentUser.get().then((userDoc) => {
+        var activityList = userDoc.data().favourites;
+        if (typeof activityList === 'undefined') {
+          $('#favourites_group').html(
+            `<div style='text-align:center;padding-top:2em'>
+                <h5> You have no favourites yet! </h5>`
+          );
+        } else if (activityList.length == 0) {
+          $('#favourites_group').html(
+            `<div style='text-align:center;padding-top:2em'> 
+                  <h5> You have no favourited activities yet! </h5>`
+          );
+        } else {
+          activityLength = activityList.length;
+          displayList('activities_example', activityLength, activityList);
+        }
+      });
+    }
+  });
 }
 
 getFavourites();
