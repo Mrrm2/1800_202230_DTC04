232fd9904dd9ada8c2e0f93e28fcb76348092fda
kirayoon <113317993+kirayoon@users.noreply.github.com>
2022-12-02 15:06:16 -0800

    Remove unnecessary comments

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -33,13 +33,8 @@ var uiConfig = {
 	signInFlow: 'popup',
 	signInSuccessUrl: '/home.html',
 	signInOptions: [
-		// Leave the lines as is for the providers you want to offer your users.
 		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-		// firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-		// firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-		// firebase.auth.GithubAuthProvider.PROVIDER_ID,
 		firebase.auth.EmailAuthProvider.PROVIDER_ID,
-		// firebase.auth.PhoneAuthProvider.PROVIDER_ID,
 	],
 	// Terms of service url.
 	tosUrl: '<your-tos-url>',
